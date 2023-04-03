77431ebe13b0532736a9965357cfc1b129e6a95b
mohamed saber <mohamedfx14@gmail.com>
2023-04-03 05:51:09 +0200

    this is full project hunt it

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomescreenComponent } from './pages/homescreen/homescreen.component';
+import { SearchscreenComponent } from './pages/searchscreen/searchscreen.component';
+import { MovieDetailsComponent } from './pages/movie-details/movie-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomescreenComponent},
+  {path:'/search',component:SearchscreenComponent},
+  {path:'/movie/:id',component:MovieDetailsComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
