e657ec377e4b324b91eaad084f65a69971c0a14f
mohamed saber <mohamedfx14@gmail.com>
2023-04-03 05:51:45 +0200

    this full project hunt it

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'showtimeApp'`, () => {
+  it(`should have as title 'showtime'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('showtimeApp');
+    expect(app.title).toEqual('showtime');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('showtimeApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('showtime app is running!');
   });
 });
---
+++ src/app/pages/homescreen/homescreen.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-homescreen',
+  templateUrl: './homescreen.component.html',
+  styleUrls: ['./homescreen.component.sass']
+})
+export class HomescreenComponent {
+
+}
